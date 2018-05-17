1d0dc221d6daad82386224afd60b90a6f9cb4ab7
Pinoshino <pino@machikoe.com>
2018-05-17 14:11:37 +0900

    query追加

--- lib/palette/elastic_search/searchable.rb
+++ lib/palette/elastic_search/searchable.rb
@@ -36,7 +36,22 @@ module Palette
                                                          settings: self.settings.to_hash,
                                                          mappings: self.mappings.to_hash
                                                        }
+          process_started_at = Time.current
           self.__elasticsearch__.import(index: new_index_name)
+          process_end_at = Time.current
+
+          # @note for new records generated while indexing
+          unless self.where(updated_at: process_started_at..process_end_at).empty?
+            loop do
+              previous_started_at = process_started_at
+              previous_end_at = process_end_at
+              process_started_at = Time.current
+              # @see https://github.com/elastic/elasticsearch-rails/blob/master/elasticsearch-model/lib/elasticsearch/model/importing.rb
+              self.__elasticsearch__.import(index: new_index_name), query: -> { where(updated_at: previous_started_at..previous_end_at) }
+              process_end_at = Time.current
+              break if self.where(updated_at: process_started_at..process_end_at).empty?
+            end
+          end
         end
 
         def create_index!
